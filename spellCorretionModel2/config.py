b2820297ff95e703669419add22f6fbab3e4cc31
FlexonaFFt <flexonafft@bk.ru>
2025-02-26 08:39:22 +0300

    --update conf 2

--- spellCorretionModel2/config.py
+++ spellCorretionModel2/config.py
@@ -1,7 +1,7 @@
 class Config:
     # Гиперпараметры модели
     input_size = None
-    hidden_size = 32
+    hidden_size = 128
     output_size = None
 
     # Гиперпараметры обучения
@@ -10,6 +10,6 @@ class Config:
     batch_size = 256
 
     # Пути для сохранения и загрузки
-    model_path = '../locmodels/spellCorrectionMdls/spell_correction_model.pth'
-    vectorizer_path = '../locmodels/spellCorrectionMdls/vectorizer.pkl'
-    label_encoder_path = '../locmodels/spellCorrectionMdls/label_encoder.pkl'
+    model_path = '../locmodels/spellCorrectionMdls2/spell_correction_model.pth'
+    vectorizer_path = '../locmodels/spellCorrectionMdls2/vectorizer.pkl'
+    label_encoder_path = '../locmodels/spellCorrectionMdls2/label_encoder.pkl'
