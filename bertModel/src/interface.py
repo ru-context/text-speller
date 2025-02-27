11bd68e4ae8a3bed19a3269aa542d50536630657
FlexonaFFt <flexonafft@bk.ru>
2025-02-27 10:06:44 +0300

    --load model tested

--- bertModel/src/interface.py
+++ bertModel/src/interface.py
@@ -1,39 +1,32 @@
-import torch
-from transformers import BertForMaskedLM, AdamW, BertTokenizer
-from utils import load_data, create_dataloader, save_model
-import sys
-import os
-
+import torch, os, sys
+from transformers import BertForMaskedLM, BertTokenizer
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import DATA_PATHS, MODEL_PATHS, TRAINING_CONFIG
 
-def train_model():
+def correct_text(model, tokenizer, text, device):
+    inputs = tokenizer(text, return_tensors='pt', max_length=128, truncation=True, padding='max_length')
+    inputs = {k: v.to(device) for k, v in inputs.items()}
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    predicted_token_ids = torch.argmax(outputs.logits, dim=-1)
+    corrected_text = tokenizer.decode(predicted_token_ids[0], skip_special_tokens=True)
+    return corrected_text
+
+def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Используется устройство: {device}")
-    model = BertForMaskedLM.from_pretrained(MODEL_PATHS['pretrained'])
-    tokenizer = BertTokenizer.from_pretrained(MODEL_PATHS['pretrained'])
-
+    print("Загрузка модели и токенизатора...")
+    model = BertForMaskedLM.from_pretrained(MODEL_PATHS['trained'])
+    tokenizer = BertTokenizer.from_pretrained(MODEL_PATHS['trained'])
     model.to(device)
-    train_data = load_data(DATA_PATHS['splits']['train'])
-    train_dataloader = create_dataloader(DATA_PATHS['splits']['train'], tokenizer, batch_size=TRAINING_CONFIG['batch_size'])
-    optimizer = AdamW(model.parameters(), lr=TRAINING_CONFIG['learning_rate'])
 
-    for epoch in range(TRAINING_CONFIG['epochs']):
-        model.train()
-        total_loss = 0
-        for batch in train_dataloader:
-            optimizer.zero_grad()
-            input_ids = batch['input_ids'].to(device)
-            attention_mask = batch['attention_mask'].to(device)
-            labels = batch['labels'].to(device)
-            outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
-            loss = outputs.loss
-            total_loss += loss.item()
-            loss.backward()
-            optimizer.step()
-        avg_loss = total_loss / len(train_dataloader)
-        print(f'Epoch {epoch+1}, Loss: {avg_loss}')
-    save_model(model, tokenizer, MODEL_PATHS['trained'])
+    while True:
+        text = input("Введите текст для исправления (или 'exit' для выхода): ")
+        if text.lower() == 'exit':
+            break
+        corrected_text = correct_text(model, tokenizer, text, device)
+        print(f"Исправленный текст: {corrected_text}")
 
 if __name__ == '__main__':
-    train_model()
+    main()
